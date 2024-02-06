47ca362c2ea6e243985067cea08b828172ebf20d
igmainc <igmainc@foxmail.com>
2024-02-06 18:50:38 +0800

    test

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,6 +2,8 @@ import type { Config } from 'drizzle-kit';
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+console.log(process.env.DB_URL);
+
 export default {
   schema: './src/schema/*',
   out: './drizzle',
